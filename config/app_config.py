b592c8bd71c4fa2484dc541e5ad3f2ee8649756b
sochoa885 <santiago439s@gmail.com>
2024-11-20 13:21:20 -0500

    Interval data

--- config/app_config.py
+++ config/app_config.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from config.warframe_data import fetch_and_update_items
 
-fetch_hours = 6
+fetch_hours = 1
 
 app = FastAPI(
     title="WarframeStatifyAPI",
